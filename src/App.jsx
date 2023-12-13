84f87364c291133d487f5ae2df8f94d7c76ccc7c
Ventsislav Kalapchiyski <yavorov1995@gmail.com>
2023-12-13 10:07:48 +0200

    todolist component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
+import TodoList from "./components/TodoList"
+import './App.css'
+
 function App() {
 
   return (
-    <>
-    <p>aide</p>
-    </>
+    <div className="App">
+      <header className="app-header">
+        <h1>My Awesome Todo App</h1>
+        <p>This is a simple React Todo app built with Vite.</p>
+      </header>
+      <main>
+        <TodoList />
+      </main>
+    </div>
   )
 }
 
---
+++ src/components/TodoList.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import styles from './TodoList.module.css';
+
+export default function TodoList() {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState('');
+
+  const addTask = () => {
+    if (newTask.trim() !== '') {
+      setTasks([...tasks, newTask]);
+      setNewTask('');
+    }
+  };
+  return (
+    <div className={styles.todoList}>
+      <h1>Todo List</h1>
+
+      <div className={styles.todoForm}>
+        <input
+          type="text"
+          placeholder="New task..."
+          value={newTask}
+          onChange={(e) => setNewTask(e.target.value)}
+        />
+        <button onClick={addTask}>Add Task</button>
+      </div>
+
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index}>{task}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
