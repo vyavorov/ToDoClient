07f57780b1d97e983cd2dfb26f5404b355766deb
Ventsislav Kalapchiyski <yavorov1995@gmail.com>
2024-01-03 14:40:26 +0200

    pagination buttons stylings

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,8 +1,11 @@
 import { useEffect, useState } from "react";
 import * as todoService from '../services/todoService';
+import styles from './Pagination.module.css';
+
 export default function Pagination({fetchData, pageState, setPageState, todos}) {
   const pageSize = 5;
   const [pagesToShow, setPagesToShow] = useState(1);
+  const [activePage, setActivePage] = useState(1);
   useEffect(() => {
     getTodosCount();
   }, [todos])
@@ -19,12 +22,13 @@ export default function Pagination({fetchData, pageState, setPageState, todos}) 
   const pageHandler = async (currentPage) => {
     fetchData(currentPage);
     setPageState(currentPage);
+    setActivePage(currentPage);
   };
   return (
-    <>
+    <div className={styles.buttonWrapper}>
       {Array.from({ length: pagesToShow }, (_, index) => (
-        <button key={index + 1} onClick={() => pageHandler(index+1)}>{index + 1}</button>
+        <button className={activePage === index + 1 ? `${styles.eachPage} ${styles.activePage}` : styles.eachPage} key={index + 1} onClick={() => pageHandler(index+1)}>{index + 1}</button>
       ))}
-    </>
+    </div>
   );
 }
