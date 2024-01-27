3393af5d8fbf3ed8dfaa3fde2bf389843f7820cb
Ventsislav Kalapchiyski <yavorov1995@gmail.com>
2024-01-27 14:27:25 +0200

    login/register components added

---
+++ src/components/Register.jsx
@@ -0,0 +1,58 @@
+import { Link } from "react-router-dom";
+import styles from './Register.module.css';
+import { useNavigate } from "react-router-dom";
+// import { useContext } from "react";
+// import AuthContext from "../../contexts/authContext";
+
+export default function Register() {
+    const [registerData, setRegisterData] = useState({
+        email: '',
+        password: '',
+    });
+
+    const onChange = (e) => {
+        const { name, value } = e.target;
+
+        setRegisterData((prevData) => ({
+            ...prevData,
+            [name]: value,
+        }));
+    };
+    return (
+        <section className={styles.auth}>
+            <form onSubmit={onSubmit}>
+                <div className={styles.container}>
+                    <div className={styles.brandLogo}></div>
+                    <h1>Register</h1>
+                    <label htmlFor="email">Email:</label>
+                    <input type="email"
+                        required
+                        name="email"
+                        id="email"
+                        placeholder="email address"
+                        onChange={onChange}
+                        value={values.email}
+                    />
+
+                    <label htmlFor="password">Password:</label>
+                    <input type="password"
+                        required
+                        name="password"
+                        id="password"
+                        placeholder="password"
+                        onChange={onChange}
+                        value={values.password}
+                    />
+
+                    {error && <p className={styles.error}>{error}</p>}
+
+
+                    <input type="submit"
+                        value="Register"
+                        className={`${styles.btn} ${styles.submit}`}
+                    />
+                </div>
+            </form>
+        </section>
+    )
+}
